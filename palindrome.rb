2b8bae202212f0e1ece94da7cfbd7a24a6d34ca3
SanaNasar <sanasehereen.cse@gmail.com>
2015-04-11 14:43:03 -0700

    Adding palindrome ruby

---
+++ palindrome.rb
@@ -0,0 +1,16 @@
+# Palindrome.rb
+# Using the Ruby language, have the function Palindrome(str) take the str parameter being passed and return the string true if the parameter is a palindrome, (the string is the same forward as it is backward) otherwise return the string false. For example: "racecar" is also "racecar" backwards. Punctuation and numbers will not be part of the string. 
+
+
+def palindrome(str)
+    string = str.downcase
+    str_reverse = str.reverse
+    
+    if str_reverse == string
+        return true
+    else
+       return false 
+    end
+end
+
+palindrome("racecar")
