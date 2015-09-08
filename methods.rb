df5720bf2cd83185ef3724d92d24b75f1d88902d
SanaNasar <sanasehereen.cse@gmail.com>
2015-09-07 22:53:25 -0700

    adding ruby methods demo

--- methods.rb
+++ methods.rb
@@ -1,10 +1,50 @@
-# demonstrating methods in ruby 
+# demonstrating methods in ruby
+# Print out that we're adding two numbers together
+# Add then return the two numbers
 
-def add(a,b)
-  puts "Adding #{a} and #{b}:"
-  puts a + b
+# def add(a,b)
+#   puts "Adding #{a} and #{b}:"
+#   return a + b # returning the sum of two numbers
+# end
+
+# puts add(2,3)
+# add(5,4)
+# add(9,9)
+
+# define a class
+class Box
+   # constructor method
+   def initialize(w,h)
+      @width, @height = w, h
+   end
+
+   # accessor methods
+   def getWidth
+      @width
+   end
+   def getHeight
+      @height
+   end
+
+   # setter methods
+   def setWidth=(value)
+      @width = value
+   end
+   def setHeight=(value)
+      @height = value
+   end
 end
 
-add(2,3)
-add(5,4)
-add(9,9)
+# create an object
+box = Box.new(10, 20)
+
+# use setter methods
+box.setWidth = 30
+box.setHeight = 50
+
+# use accessor methods
+x = box.getWidth()
+y = box.getHeight()
+
+puts "Width of the box is : #{x}"
+puts "Height of the box is : #{y}"
